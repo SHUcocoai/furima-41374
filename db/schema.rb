ba8dc119e4022c3bbb8f1a7a5d242b8bf9e2d672
SHUcocoai <chongjieyiteng@gmail.com>
2024-08-23 21:47:01 +0900

    サインアップ部分のビューの編集

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_23_070330) do
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "birth_date", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
