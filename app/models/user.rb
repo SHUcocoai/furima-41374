eedc9ad625b24aac226a817144bb53be3679d8a6
SHUcocoai <chongjieyiteng@gmail.com>
2024-08-25 12:13:45 +0900

    バリテーションの正規表現の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :password, format: { 
+    with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-zA-Z\d]+\z/i, 
+    message: "は半角英字と数字をそれぞれ1文字以上含む必要があります"
+  }
+
+  with_options presence: { message: "を入力してください" },
+             format: { 
+               with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, 
+               message: "は全角文字（ひらがな、カタカナ、漢字）で入力してください"
+             } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: { message: "を入力してください" },
+             format: { 
+               with: /\A[ァ-ヶ]+\z/, 
+               message: "は全角文字（カタカナ）で入力してください"
+             } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
   validates :birth_date, presence: true
 end
