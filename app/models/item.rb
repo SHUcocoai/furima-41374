73c3f349c9620d4dd25ba11898a794b9af86b112
SHUcocoai <chongjieyiteng@gmail.com>
2024-08-27 12:19:53 +0900

    バリテーション追加記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,19 +8,19 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
 
-  validates :image, presence: true
-  validates :item_name, presence: true
-  validates :description, presence: true
-  validates :item_category_id, presence: true
-  validates :item_status_id, presence: true
-  validates :shipping_fee_status_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_date_id, presence: true
-  validates :price, presence: true
-  validates :item_category_id, numericality: { other_than: 1, message: "Item category can't be blank" }
-  validates :item_status_id, numericality: { other_than: 1, message: "Item status can't be blank" }
-  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "Shipping fee status can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "Prefecture can't be blank" }
-  validates :shipping_date_id, numericality: { other_than: 1, message: "Shipping date can't be blank" }
-  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  with_options presence: true do
+  validates :image
+  validates :item_name
+  validates :description
+  validates :price
+  end
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :item_category_id
+    validates :item_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end  
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "must be a half-width number between 300 and 9,999,999" }
+  validates :price, format: { with: /\A[0-9]+\z/, message: "must be a half-width number" }
 end
