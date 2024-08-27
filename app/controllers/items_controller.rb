f3931a54d211df4da472d6b41256fb5608974a87
SHUcocoai <chongjieyiteng@gmail.com>
2024-08-27 16:49:22 +0900

    出品画面の表示とitemsコントローラーの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,20 @@ class ItemsController < ApplicationController
   def index
   end
   
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to root_path
+  end
+
+
+  private
+
+  def item_params
+    prams.require(:item).permit(:image, :item_name, :description, :item_category_id, :item_status_id, :shipping_fee_status_id, :prefecture_id, :shipping_date_id, :price ).merge(user_id: current_user.id)
+  end
 end
+
