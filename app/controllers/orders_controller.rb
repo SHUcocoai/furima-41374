4c0a106b7b115dc21c5cbf726bc003bd08cc6156
SHUcocoai <chongjieyiteng@gmail.com>
2024-09-04 21:07:29 +0900

    テストモードの公開鍵とトークンの作成とサーバーへの送信の記述、秘密鍵の記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,12 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new(order_shipping_address_params)
     if @order_shipping_address.valid?
+      Payjp.api_key = "sk_test_c37d1222b17a33dad4041da7"
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_shipping_address_params[:token],
+        currency: 'jpy'
+      )
        @order_shipping_address.save
        redirect_to root_path
     else 
@@ -18,6 +24,6 @@ class OrdersController < ApplicationController
 
   private
   def order_shipping_address_params
-    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :order).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :order).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
   end
 end
--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,6 +1,6 @@
 class OrderShippingAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :order, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do 
   validates :postal_code, format: {with: /\A\d{3}-\d{4}\z/, message: "enter in the format '3 digits-4 digits'." }
